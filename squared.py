027311e3a93eb2ed873aa6c3041d3bf634df5b2e
Brandon Evans <bevans@capitolscientific.com>
2019-09-17 09:17:06 -0500

    add more function practice

---
+++ squared.py
@@ -0,0 +1,7 @@
+def square(number):
+    return number*number
+
+number = int(input("Pick a number "))
+squared_num = square(number)
+
+print("The square root of {} is {}".format(number, squared_num))
