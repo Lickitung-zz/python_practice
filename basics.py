61eae8eb9a3a2ce1b6d10d0d7bedd2417a69a0df
Brandon Evans <bevans@capitolscientific.com>
2019-09-17 08:54:44 -0500

    add first function

--- basics.py
+++ basics.py
@@ -1,4 +1,12 @@
-print(int("11"))
-print(float(11))
-number_of_burrito_characters = len("burrito")
-print(number_of_burrito_characters)
+# print(int("11"))
+# print(float(11))
+# number_of_burrito_characters = len("burrito")
+# print(number_of_burrito_characters)
+
+def yell(text):
+    text = text.upper()
+    number_of_characters = len(text)
+    result = text + "!" * (number_of_characters // 2)
+    print(result)
+
+yell("I did a python thingy")
