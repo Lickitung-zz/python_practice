eb55b0aa9d0c88ac4bc7e3bfb70bbd2503b1d659
Brandon Evans <brandon_n_evans@live.com>
2019-09-18 18:50:54 -0500

    add random test file and add more practice

--- wishlist.py
+++ wishlist.py
@@ -7,6 +7,20 @@ books = [
     "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
 ]
 
-del books[0]
+video_games = [
+    "The Legend of Zelda: Breath of the Wild",
+    "Splatoon 2",
+    "Super Mario Odyssey",
+]
 
-print(books)
+def display_wishlist(display_name, wishes):
+    items = wishes.copy()
+    print(display_name + ":")
+    suggested_gift = items.pop(0)
+    print("======>", suggested_gift, "<=======")
+    for item in items:
+        print("* " + item)
+    print()
+
+display_wishlist("Books", books)
+display_wishlist("Video Games", video_games)
