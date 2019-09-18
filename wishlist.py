02909713afc2c73dc93ee15b0f521200250a2fa7
Brandon Evans <bevans@capitolscientific.com>
2019-09-18 15:34:42 -0500

    update wishlist

---
+++ wishlist.py
@@ -0,0 +1,12 @@
+books = [
+    "Learning Python: Powerful Object-Oriented Programming - Mark Lutz",
+    "Automate the Boring Stuff with Python: Practical Programming for Total Beginners - Al Sweigart",
+    "Python for Data Analysis - Wes McKinney",
+    "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
+    "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
+    "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
+]
+
+del books[0]
+
+print(books)
