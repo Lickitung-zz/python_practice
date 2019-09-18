99d187bfcc0d087e413d1409fd2ced0e77f5eb22
Brandon Evans <bevans@capitolscientific.com>
2019-09-18 09:30:12 -0500

    refactor and add service_charge const

--- masterticket.py
+++ masterticket.py
@@ -1,21 +1,22 @@
 TICKET_PRICE = 10
+SERVICE_CHARGE = 2
 
 tickets_remaining = 100
+
+def calculate_price(ticket_quantity):
+    return (ticket_quantity * TICKET_PRICE) + SERVICE_CHARGE
 
 while tickets_remaining > 0:
     print("\nThere are currently {} tickets remaining.".format(tickets_remaining))
     name = input("What is your name?   ")
-    # expect a ValueError to happen and handle it
     try:
         ticket_quantity = int(input("Hey {}! How many tickets would you like?    ".format(name)))
-        # Raise a ValueError if the request is for more tickets than are available
         if ticket_quantity > tickets_remaining:
             raise ValueError("There are only {} tickets remaining".format(tickets_remaining))
     except ValueError as err:
-        # Include err text in output
         print("\nOops! We ran into an issue. {}. Try again...".format(err))
     else:
-        amount_due = ticket_quantity * TICKET_PRICE
+        amount_due = calculate_price(ticket_quantity * TICKET_PRICE)
         print("\nAlright {}, your total is going to be ${}!".format(name, amount_due))
         to_proceed = input("Is this amount ok?  (Y/N)    ").lower()
 
