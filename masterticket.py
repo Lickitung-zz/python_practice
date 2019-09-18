7e670c207956b9bcd829761d1ad640afb204b1a5
Brandon Evans <bevans@capitolscientific.com>
2019-09-18 09:21:33 -0500

    add ValueError exception

--- masterticket.py
+++ masterticket.py
@@ -2,36 +2,26 @@ TICKET_PRICE = 10
 
 tickets_remaining = 100
 
-# Run this code continuously until we run out of tickets
 while tickets_remaining > 0:
-    # Output how many tickets are remaining using the tickets_remaining variable
     print("\nThere are currently {} tickets remaining.".format(tickets_remaining))
-
-    # Gather the user's name and assign it to a new variable
     name = input("What is your name?   ")
-
-    # Prompt the user by name and ask how many tickets they would like
-    ticket_quantity = int(input("Hey {}! How many tickets would you like?    ".format(name)))
-
-    # Calculate the price (num of tickets * price) and assign it to a variable
-    amount_due = ticket_quantity * TICKET_PRICE
-
-    # Output the price to the screen
-    print("\nAlright {}, your total is going to be ${}!".format(name, amount_due))
-
-    # Prompt user if they want to proceed. Y/N?
-    to_proceed = input("Is this amount ok?  (Y/N)    ").lower()
-
-    # If they want to proceed
-    if to_proceed == "y":
-        # Print out to the screen "SOLD!" to confirm purchase
-        print("SOLD!")
-        # and then decrement the tickets remaining by the num of tix purchased
-        tickets_remaining -= ticket_quantity
-    # Else ...
+    # expect a ValueError to happen and handle it
+    try:
+        ticket_quantity = int(input("Hey {}! How many tickets would you like?    ".format(name)))
+        # Raise a ValueError if the request is for more tickets than are available
+        if ticket_quantity > tickets_remaining:
+            raise ValueError("There are only {} tickets remaining".format(tickets_remaining))
+    except ValueError as err:
+        # Include err text in output
+        print("\nOops! We ran into an issue. {}. Try again...".format(err))
     else:
-        # Thank them by name
-        print("Thanks anyways {}.".format(name))
+        amount_due = ticket_quantity * TICKET_PRICE
+        print("\nAlright {}, your total is going to be ${}!".format(name, amount_due))
+        to_proceed = input("Is this amount ok?  (Y/N)    ").lower()
 
-# Notify user that the tickets are sold out
+        if to_proceed == "y":
+            print("SOLD!")
+            tickets_remaining -= ticket_quantity
+        else:
+            print("Thanks anyways {}.".format(name))
 print("We're out of tickets!")
