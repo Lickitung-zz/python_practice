30f10a215a024d429c12e1b5ff2b0f7f03d91bc9
Brandon Evans <bevans@capitolscientific.com>
2019-09-19 09:29:43 -0500

    add manual insertion option within add_to_list function, add error handling

--- random_test_file.py
+++ random_test_file.py
@@ -1,11 +1,17 @@
-travel_expenses = [
-    [5.00, 2.75, 22.00, 0.00, 0.00],
-    [24.75, 5.50, 15.00, 22.00, 8.00],
-    [2.75, 5.50, 0.00, 29.00, 5.00],
+musical_groups = [
+    ["Ad Rock", "MCA", "Mike D."],
+    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
+    ["Salt", "Peppa", "Spinderella"],
+    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
+    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
+    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
+    ["Run", "DMC", "Jam Master Jay"],
 ]
+# Your code here
+i = 0
+group_members = []
+for group in musical_groups:
+    if len(group) == 3:
+        print(", ".join(group))
 
-print("Travel Expenses: ")
-week_number = 1
-for week in travel_expenses:
-    print("* Week #{}: ${}".format(week_number, sum(week)))
-    week_number += 1
+
