230db55e30dbc95f2ecb4d392f174f8e5f35bc80
Brandon Evans <brandon_n_evans@live.com>
2019-09-20 18:15:26 -0500

    add practice and refactor shoppinglist with tuple

--- random_test_file.py
+++ random_test_file.py
@@ -1,17 +1,18 @@
-musical_groups = [
-    ["Ad Rock", "MCA", "Mike D."],
-    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
-    ["Salt", "Peppa", "Spinderella"],
-    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
-    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
-    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
-    ["Run", "DMC", "Jam Master Jay"],
-]
-# Your code here
-i = 0
-group_members = []
-for group in musical_groups:
-    if len(group) == 3:
-        print(", ".join(group))
+# I need you to create a new function for me.
+
+# This one will be named sillycase and it'll take a single string as an argument.
+
+# sillycase should return the same string but the first half should be lowercased
+# and the second half should be uppercased. 
+# For example, with the string \"Treehouse\", sillycase would return \"treeHOUSE\".
+
+# Don't worry about rounding your halves, but remember that indexes should be integers. 
+# You'll want to use the int() function or integer division, //."
 
 
+
+def sillycase(str):
+    return str[:(len(str) // 2)].lower() + str[(len(str) // 2):(len(str))].upper()
+
+
+print(sillycase("apple"))
