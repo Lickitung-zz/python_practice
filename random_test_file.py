a78561ad147e7b9d0b6dc3f1cbf528df9dfd714d
Brandon Evans <bevans@capitolscientific.com>
2019-09-20 09:29:14 -0500

    add challenge problem

--- random_test_file.py
+++ random_test_file.py
@@ -1,17 +1,13 @@
-musical_groups = [
-    ["Ad Rock", "MCA", "Mike D."],
-    ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"],
-    ["Salt", "Peppa", "Spinderella"],
-    ["Rivers Cuomo", "Patrick Wilson", "Brian Bell", "Scott Shriner"],
-    ["Chuck D.", "Flavor Flav", "Professor Griff", "Khari Winn", "DJ Lord"],
-    ["Axl Rose", "Slash", "Duff McKagan", "Steven Adler"],
-    ["Run", "DMC", "Jam Master Jay"],
-]
-# Your code here
-i = 0
-group_members = []
-for group in musical_groups:
-    if len(group) == 3:
-        print(", ".join(group))
+# E.g. word_count("I do not like it Sam I Am") gets back a dictionary like:
+# {'i': 2, 'do': 1, 'it': 1, 'sam': 1, 'like': 1, 'not': 1, 'am': 1}
+# Lowercase the string to make it easier.
 
+def word_count(string):
+    dictionary = {}
+    list_with_string = string.lower().split() # small chars in list
+    for item in list_with_string:
+        value = list_with_string.count(item)
+        dictionary[item] = value
+    return dictionary
 
+print(word_count("I do not like it Sam I am"))
