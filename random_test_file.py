44df2a446b71dc9518befad365b18ba2b3802378
Brandon Evans <bevans@capitolscientific.com>
2019-09-23 08:47:04 -0500

    add basic planning for dungeon_game

--- random_test_file.py
+++ random_test_file.py
@@ -1,17 +1,1 @@
-# Let's play with the *args pattern.
-
-# Create a function named multiply that takes any number of arguments. 
-# Return the product (multiplied value) of all of the supplied arguments. 
-# The type of argument shouldn't matter.
-
-# Slices might come in handy for this one.
-
-def multiply(*args):
-    sum = 0
-    i = 0
-    arg_len = len(*args)
-    for arg in [*args]:
-        print(arg[i])
-
-numbers = [1, 2, 3, 4, 5, 6]
-multiply(numbers)
+print({3.3, 3.4, 3.5, 2.7} ^ {2.6, 2.5, 2.7, 3.4})
