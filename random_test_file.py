a884e183e23898061d1b9db3ed0ca969ecc7a6db
Brandon Evans <bevans@capitolscientific.com>
2019-09-23 07:47:59 -0500

    merge

--- random_test_file.py
+++ random_test_file.py
@@ -1,47 +1,17 @@
-# The dictionary will look something like:
-# {'Andrew Chalkley': ['jQuery Basics', 'Node.js Basics'],
-#  'Kenneth Love': ['Python Basics', 'Python Collections']}
-#
-# Each key will be a Teacher and the value will be a list of courses.
-#
-# Your code goes below here.
+# Let's play with the *args pattern.
 
-dictionary = {"Obama": ["Python Basics", "JavaScript Basics", "HTML/CSS", "GoLang"],
-              "Trump": ["jQuery Basics", "node.js basics", "EmojiCode"],
-              "Kennedy": ["C# Basics", "UNIX"]}
+# Create a function named multiply that takes any number of arguments. 
+# Return the product (multiplied value) of all of the supplied arguments. 
+# The type of argument shouldn't matter.
 
-def num_teachers(dict):
-    return len(dict.keys())
+# Slices might come in handy for this one.
 
-def num_courses(dict):
-    num_of_courses = 0
-    courses = [*dict.values()]
-    for course in courses:
-        num_of_courses += len(course)
-    return num_of_courses
+def multiply(*args):
+    sum = 0
+    i = 0
+    arg_len = len(*args)
+    for arg in [*args]:
+        print(arg[i])
 
-def courses(dict):
-    course_list = []
-    courses = [*dict.values()]
-    for course in courses:
-        course_list.extend(course)
-    return course_list
-
-def most_courses(dict):
-    max_count = 0
-    longest_teacher = ''
-    for teacher in dict:
-        if len(dict[teacher]) > max_count:
-            max_count = len(dict[teacher])
-            longest_teacher = teacher
-    return longest_teacher
-
-def stats(dict):
-    num_courses = 0
-    new_list = []
-    for teacher in dict:
-        num_courses = len(dict[teacher])
-        new_list += [[teacher, num_courses]]
-    return print(new_list)
-
-print(stats(dictionary))
+numbers = [1, 2, 3, 4, 5, 6]
+multiply(numbers)
