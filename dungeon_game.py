c25e56721f9521c62c97c12884ae8736925b58a9
Brandon Evans <bevans@capitolscientific.com>
2019-09-24 09:08:05 -0500

    add ability to move player

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 # draw grid
 # pick random location for player
@@ -18,39 +19,67 @@ CELLS = [
     (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)
 ]
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+CELLS.copy()
+
 def get_locations():
-    monster = None
-    door = None
-    player = None
+    return random.sample(CELLS, 3)
 
     return monster, door, player
 
 def move_player(player, move):
     # get the player's location
+    x, y = player
+    if move == "LEFT":
+        x -= 1
+    if move == "RIGHT":
+        x += 1
+    if move == "UP":
+        y -= 1
+    if move == "DOWN":
+        y += 1
     # if move == LEFT, x - 1
     # if move == RIGHT, x + 1
     # if move == Up, y - 1
     # if move == DOWN, y + 1
-    return player
+    return x, y
 
 def get_moves(player):
     moves = ["LEFT", "RIGHT", "UP", "DOWN"]
+    x, y = player
+    if x == 0:
+        moves.remove("LEFT")
+    if x == 4:
+        moves.remove("RIGHT")
+    if y == 0:
+        moves.remove("UP")
+    if y == 4:
+        moves.remove("DOWN")
+    return moves
     # if player's y == 0, they can't move up
     # if player's y == 4, they can't mvoe down
-    # if player's x == 0, they can't move left
-    # if player's x == 4, they can't move right
 
+monster, door, player = get_locations()
+print("The monster is at {}, and the door is at {}".format(monster, door))
 while True:
+    valid_moves = get_moves(player)
+    clear_screen()
     print("Welcome to the dungeon!")
-    print("You're currently in room {}") # fill with player position
-    print("You can move {}") #fill with available moves
+    print("You're currently in room {}".format(player)) # fill with player position
+    print("You can move {}".format(", ".join(valid_moves))) #fill with available moves
     print("Enter QUIT to quit")
 
     move = input("> ").upper()
 
     if move == 'QUIT':
         break
-
+    if move in valid_moves:
+        player = move_player(player, move)
+    else:
+        print("\n ** Walls are hard! Don't run into them! **\n")
+        continue
     # Good move? Change the player pos
     # Bad move? Don't change anything
     # On the door? They win!
