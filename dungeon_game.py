661b84a2275394400c8075a4230cf1957d7db878
Brandon Evans <bevans@capitolscientific.com>
2019-09-30 08:26:59 -0500

    add grid map

--- dungeon_game.py
+++ dungeon_game.py
@@ -56,30 +56,48 @@ def get_moves(player):
     if y == 4:
         moves.remove("DOWN")
     return moves
-    # if player's y == 0, they can't move up
-    # if player's y == 4, they can't mvoe down
 
-monster, door, player = get_locations()
-print("The monster is at {}, and the door is at {}".format(monster, door))
-while True:
-    valid_moves = get_moves(player)
-    clear_screen()
-    print("Welcome to the dungeon!")
-    print("You're currently in room {}".format(player)) # fill with player position
-    print("You can move {}".format(", ".join(valid_moves))) #fill with available moves
-    print("Enter QUIT to quit")
+def draw_map(player):
+    print(" _" * 5)
+    tile = "|{}"
 
-    move = input("> ").upper()
+    for cell in CELLS:
+        x, y = cell
+        if x < 4:
+            line_end = ""
+            if cell == player:
+                output = tile.format("X")
+            else:
+                output = tile.format("_")
+        else:
+            line_end = "\n"
+            if cell == player:
+                output = tile.format("X|")
+            else:
+                output = tile.format("_|")
+        print(output, end = line_end)
 
-    if move == 'QUIT':
-        break
-    if move in valid_moves:
-        player = move_player(player, move)
-    else:
-        print("\n ** Walls are hard! Don't run into them! **\n")
-        continue
-    # Good move? Change the player pos
-    # Bad move? Don't change anything
-    # On the door? They win!
-    # On the monster? They lose!
-    # Otherwise, loop back around
+def game_loop():
+    monster, door, player = get_locations()
+    while True:
+        draw_map(player)
+        valid_moves = get_moves(player)
+        print("You're currently in room {}".format(player)) # fill with player position
+        print("You can move {}".format(", ".join(valid_moves))) #fill with available moves
+        print("Enter QUIT to quit")
+
+        move = input("> ").upper()
+
+        if move == 'QUIT':
+            break
+        if move in valid_moves:
+            player = move_player(player, move)
+        else:
+            input("\n ** Walls are hard! Don't run into them! **\n")
+        clear_screen()
+        
+clear_screen()
+print("Welcome to the dungeon!")
+input("Press return to start!")
+clear_screen()
+game_loop()
